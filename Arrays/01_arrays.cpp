1cd7ec533feb0f4b01fa564e037cf3e56b938d0e
Lakshay Chandna <lakshay13chandna@gmail.com>
2025-06-13 02:01:26 +0530

    Initial commit

---
+++ Arrays/01_arrays.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+}
+
+int main(){
+
+    int number[15];
+    // cout<< number[0]<<endl;
+
+    cout<<endl<<"Everything is fine: ";
+    return 0;
+}
